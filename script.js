626376e962873e03a068035f5616ac4eefe2f9c7
Igor Garchenok <igorgarchenok@yandex.ru>
2022-12-23 19:39:07 +0300

    done

--- script.js
+++ script.js
@@ -21,5 +21,24 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
+"use strict";
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+const filmScore = prompt("На сколько оцените его?", "");
+const lastFilm2 = prompt("Один из последних просмотренных фильмов?", "");
+const filmScore2 = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[lastFilm] = filmScore;
+personalMovieDB.movies[lastFilm2] = filmScore2;
+
+console.log(personalMovieDB);
